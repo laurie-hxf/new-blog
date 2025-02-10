10db59f32df6cb2b89d3071ccbf920278ec0b479
laurie-hxf <1847238294@qq.com>
2025-02-10 17:52:23 +0800

    new blog

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,9 +3,9 @@ import type { CardListData, Config, IntegrationUserConfig, ThemeUserConfig } fro
 export const theme: ThemeUserConfig = {
   // === Basic configuration ===
   /** Title for your website. Will be used in metadata and as browser tab title. */
-  title: 'Astro Theme Pure',
+  title: 'Laurie\'s Site',
   /** Will be used in index page & copyright declaration */
-  author: 'Pure Lab',
+  author: 'Laurie',
   /** Description metadata for your website. Can be used in page metadata. */
   description: 'Stay hungry, stay foolish',
   /** The default favicon for your site which should be a path to an image in the `public/` directory. */
@@ -59,8 +59,8 @@ export const theme: ThemeUserConfig = {
   footer: {
     // Registration information for ICP (optional)
     registration: {
-      url: 'https://icp.gov.moe/?keyword=APTX4869',
-      text: 'Moe ICP APTX4869'
+      url: 'https://google.com',
+      text: '粤ICP备2025369833号'
     },
     /** Enable displaying a “Astro & Pure theme powered” link in your site’s footer. */
     credits: true,
