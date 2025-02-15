99d10deac7a6e8b01964920109bce1dbef6217d1
laurie-hxf <1847238294@qq.com>
2025-02-15 16:48:59 +0800

    favicon

--- src/site.config.ts
+++ src/site.config.ts
@@ -92,8 +92,8 @@ export const integ: IntegrationUserConfig = {
     applyTip: [
       { name: 'Name', val: theme.title },
       { name: 'Desc', val: theme.description || 'Null' },
-      { name: 'Link', val: 'https://astro-pure.js.org/' },
-      { name: 'Avatar', val: 'https://astro-pure.js.org/favicon/favicon.ico' }
+      { name: 'Link', val: 'https://laurie-hxf.xyz/' },
+      { name: 'Avatar', val: 'https://laurie-hxf.xyz/favicon/favicon.ico' }
     ]
   },
   // Enable page search function
